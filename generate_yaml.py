89b92d0205c54c2d94153a949e5967f5acb88a97
phrosen2 <69522417+phrosen2@users.noreply.github.com>
2026-04-16 12:38:21 -0500

    Refactor test file naming and output validation
    Updated test file naming convention and improved test output handling.

--- generate_yaml.py
+++ generate_yaml.py
@@ -26,26 +26,44 @@ jobs:
         run: javac *.java
 """)
 
+tests = []
 for input_file in inputs:
-    num = input_file.replace("input", "").replace(".txt", "")
-    output_file = f"{num}expected.txt"
+    num = input_file.replace("input", "").replace(".txt", "").replace("_","")
+    output_file = f"{num}_expected.txt"
+    tests.append(f"test{num}")
 
     print(f"""
       - name: test{num} ({POINTS_PER_TEST} pt)
+        id: test{num}
         uses: classroom-resources/autograding-command-grader@v1
         with:
           test-name: test{num}
           command: |
-            set -e
             java TempConverter < tests/{input_file} > actual.txt
-            if diff --strip-trailing-cr actual.txt tests/{output_file}; then
+            # Extract the expected value from your file
+            EXPECTED_VAL=$(cat tests/{output_file})
+            # Search for that value inside the student's output
+            if grep -q -- "$EXPECTED_VAL" actual.txt; then
               echo "PASS"
             else
+              echo "FAIL: Could not find $EXPECTED_VAL in output."
+              echo "Student output was:"
+              cat actual.txt
               exit 1
             fi
           timeout: 2
           max-score: {POINTS_PER_TEST}
 """)
 
-
-#bash -c 'diff <(java TempConverter < tests/{input_file}) tests/{output_file}'
+print(f"""
+      - name: Autograding Reporter
+        uses: classroom-resources/autograding-grading-reporter@v1
+        env:""")
+x = 1
+for test in tests:
+    print(f"""          TEST{x}_RESULTS: "${{{{steps.{test}.outputs.result}}}}" """)
+    x += 1
+print(f"""
+        with:
+          runners: """, end="")
+print(",".join(tests), end="")
