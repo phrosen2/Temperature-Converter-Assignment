b0ef8e218523dc455a48dbd51aa55e965acc876b
phrosen2 <69522417+phrosen2@users.noreply.github.com>
2026-04-16 08:26:06 -0500

    Fix test input file naming and improve diff command

--- run_tests.sh
+++ run_tests.sh
@@ -7,25 +7,30 @@ javac *.java
 score=0
 total=0
 
-for i in tests/input*.txt; do
+for i in tests/*input.txt; do
     num=$(basename $i | grep -o '[0-9]*')
-    expected="tests/expected_${num}.txt"
+    expected="tests/${num}_expected.txt"
     total=$((total + 1))
 
     actual=$(java TempConverter < "$i")
 
-    if diff <(echo "$actual") "$expected" > /dev/null; then
+    # diff needs the --strip-trailing-cr to deal with Unix/Windows confusion in output
+    if diff --strip-trailing-cr <(echo "$actual") "$expected" > /dev/null; then
         echo "Test $num: PASS"
         score=$((score + 1))
     else
         echo "Test $num: FAIL"
+        echo "Expected: "
+        cat $expected
+        echo "Actually got:"
+        echo $actual
     fi
 done
 
 #Code Checks
 echo "Checking Scanner..."
 total=$((total + 1))
-if grep -q "Scanner" PA03.java; then
+if grep -q "Scanner" TempConverter.java; then
     echo "Scanner: PASS (+1)"
     score=$((score + 1))
 else
